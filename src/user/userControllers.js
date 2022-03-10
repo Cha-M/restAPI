c18f9e751423901503da910ca5183095e9f42351
Sha <charlotte.megroff@outlook.com>
2022-03-10 12:02:23 +0000

    Updates

--- src/user/userControllers.js
+++ src/user/userControllers.js
@@ -3,76 +3,24 @@ const User = require("./userModel");
 
 //Submission is CRUD
 
-// //Create
-// exports.addMovie = async (req, res) => {
-//     try {
-//         const newMovie = await Movie.create(req.body);
-//         res.status(200).send({ movie: newMovie });
+//List/Read
+exports.listUsers = async (req, res) => {
+    try {
+        const users = await User.find({});
+        res.status(200).send({ allUser: users });
 
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({ err: error.message });
-//     }
-// }
+        } catch (error) {
+            console.log(error);
+    }
+}
 
-// //Update
-// exports.updateMovie = async (req, res) => {
-//     try {
-//         const updatedMovie = await Movie.updateOne(
-//             {[req.body.filterKey]: req.body.filterVal},
-//             {[req.body.updateKey]: req.body.updateVal},
-            
-//         );
-//         if (updatedMovie.modifiedCount > 0) {
-//             res.status(200).send({ movie: updatedMovie });
-//         }
-//         else {
-//             throw new Error("Did not update.");
-//         }
-
-
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({ err: error.message });
-//     }
-// }
-
-// //Delete
-// exports.deleteMovie = async (req, res) => {
-//     try {
-//         const deleteMovie = await Movie.deleteOne( {[req.params.deleteKey]: req.body.deleteVal} );
-//         if (deleteMovie.modifiedCount > 0) {
-//             res.status(200).send({ movie: deleteMovie });
-//         }
-//         else {
-//             throw new Error("Did not delete.");
-//         }
-
-//     } catch (error) {
-//         console.log(error);
-//         res.status(500).send({ err: error.message });
-//     }
-// }
-
-
-// //List/Read
-// exports.listMovies = async (req, res) => {
-//     try {
-//         const movies = await Movie.find({});
-//         res.status(200).send({ allMovie: movies });
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
-
-
-
+//Create
 exports.addUser = async (req, res) => {
     try {
         const newUser = await User.create(req.body);
         const token = await jwt.sign({ _id: newUser._id }, process.env.SECRET);
         res.status(200).send({ user: newUser.username, token });
-
+        
     } catch (error) {
         console.log(error);
         res.status(500).send({ err: error.message });
@@ -80,32 +28,52 @@ exports.addUser = async (req, res) => {
     }
 }
 
-exports.login = async (req, res) => {
-    try {
-        res.status(200).send({ user: req.user.username });
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ err: error.message });
-    }
-}
-
+//Update
 exports.updatePassword = async (req, res) => {
     try {
-        // req.user
-        // req.body.password
         const updatedUser = await User.updateOne(
             { username: req.user.username },
             { password: req.body.password }
-
+            
         );
         if (updatedUser.modifiedCount > 0) {
             res.status(200).send({ msg: "Successfully updated user." });
         }
         else {
             throw new Error("Did not update.");
+        }            
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ err: error.message });
         }
+}
 
+//Delete
+exports.deleteUser = async (req, res) => {
+    try {
+        const deletedUser = await User.deleteOne({ [req.params.filterKey]: req.body.filterVal });
+            
+        // const token = await jwt.sign({ _id: deletedUser._id }, process.env.SECRET);
+        // res.status(200).send({ user: deletedUser.username, token });
 
+        if (deletedUser.deletedCount > 0) {
+            res.status(200).send({ msg: "Successfully deleted user." });
+        }
+        else {
+            throw new Error("Did not delete.");
+        }
+            
+        } catch (error) {
+            console.log(error);
+            res.status(500).send({ err: error.message });
+    }
+}
+    
+    
+exports.login = async (req, res) => {
+    try {
+        res.status(200).send({ user: req.user.username });
     } catch (error) {
         console.log(error);
         res.status(500).send({ err: error.message });
